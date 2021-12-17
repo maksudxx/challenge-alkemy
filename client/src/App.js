e1cd27c39aede4e1a9b2e0cb0b157f9dd04fea80
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-12-17 18:28:56 -0300

    react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './views/home/Home';
 import { Routes, Route } from "react-router-dom";
+import Login from './views/login/Login';
 
 function App() {
   return (
     <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
-        <Route path='home' element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path='/home' element={<Home />} />
     </Routes>
   );
 }
---
+++ client/src/views/login/Login.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+    return(
+        <Link to='/home'>Home</Link>
+    )
+}
