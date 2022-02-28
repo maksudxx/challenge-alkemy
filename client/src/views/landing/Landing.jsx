770fbeeba3a827bb6de3e4c18dbf4c1d36719c7b
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-02-28 16:02:39 -0300

    landing pages and styles

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
+import styles from "./Landing.module.css";
 
 export default function Landing() {
-    return(
-        <Link to='/home'>Home</Link>
-    )
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>BUDGET MANAGER</h1>
+      <Link to="/home" className={styles.link}>
+        <div className={styles.buttonStart}>
+          <p className={styles.p}>START</p>
+        </div>
+      </Link>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
